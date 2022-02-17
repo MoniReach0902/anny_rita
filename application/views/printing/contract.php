662bea91a1d8b4f72827dd6dc4d3a432c8d12dd6
MoniReach0902 <monireach090200@gmail.com>
2022-02-17 16:51:39 +0700

    update 17-02-22

--- application/views/printing/contract.php
+++ application/views/printing/contract.php
@@ -1,6 +1,9 @@
 <style>
-    div {
-        font-family: 'khmer os system';
+    span {
+        font-family: 'Khmer OS Muol Light';
+        font-size: 21px;
+        text-decoration-style: dotted;
+
     }
 </style>
 <div class="content-wrapper">
@@ -8,33 +11,153 @@
     <section class="content">
         <div class="box box-solid bg-dark">
             <div class="box-header with-border">
-                <h3 class="box-title"><span class="khmer_font"> បោះពុម្ភឯកសារពលករ </span> &#921; Prining Worker Bio </h3>
+                <h3 class="box-title"><span class="khmer_font"> </span> &#921;</h3>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
-                <section class="invoice printableArea" style="width=793px">
-                    <div class=" row no-print">
-                        <div class="col-12">
-                            <button id="print" class="btn btn-rounded btn-primary pull-right" type="button"> <span><i class="fa fa-print"></i> Print</span> </button>
+                <div class=" row no-print">
+                    <div class="col-12">
+                        <button id="print" class="btn btn-rounded btn-primary pull-right" type="button"> <span><i class="fa fa-print"></i> Print</span> </button>
+                    </div>
+                </div>
+                <section class="invoice printableArea">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img src="<?php echo base_url('images/1.png') ?>" alt="" width="98%">
                         </div>
                     </div>
-                    <div>
-                        <p style="font-size: 18px; font-family:'Khmer OS system'">លេខ៖11ARM </p>
-                        <p style="font-family:Khmer OS Muol Light; font-size:18px;text-align: center">
-                            ឧបសម្ព័ន្ធ<br>
-                            នៃប្រកាសលេខ ២៥៣ក.ប/ប្រក ចុះថ្ងៃទី២៣ ខែកញ្ញា ឆ្នាំ២០១៣ ស្តីពីការដាក់ <br>
-                            ឱ្យអនុវត្តបទដ្ឋានអប្បបរមាស្តីពីកិច្ចសន្យាសេវារការងារឱ្យធ្វើនៅបរទេស <br><br>
-                            បទដ្ឋានអប្បបរមា<br>
-                            ស្តីពី<br>
-                            កិច្ចសន្យាសេវារកការងារឱ្យធ្វើ សម្រាប់ពលករខ្មែរទៅធ្វើការនៅប្រទេសថៃ<br><br> </p>
-                        <p style="text-align: center; font-size: 18px ;"> កិច្ចសន្យានេះធ្វើឡើងរវាង</p>
-                        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ក្រុមហ៊ុន <span class="p1"> អាន់នី រីតា បេស មេនផៅវ័រ </span> អាសយដ្ឋានផ្ទះលេខ <span class="p1"> ៣២-៣៣អឺ </span> ផ្លូវ <span class="p1"> វេងស្រេង </span> ឃុំ&#47;សង្កាត់ <span class="p1"> ចោមចៅ </span> ស្រុក&#47;ខ័ណ្ឌ&#47;ក្រុង <span class="p1"> ពោធិ៍សែនជ័យ </span> ខេត្ត&#47;រាជធានី <span class="p1"> ភ្នំពេញ </span> ព្រះរាជាណាចក្រកម្ពុជា ដែលទទួលបានការអនុញ្ញាតតាមប្រកាសលេខ <span class="p1"> ១២៦&#47;២០ </span> ចុះថ្ងៃទី <span class="p1"> ៣០ </span> ខែ <span class="p1"> មិនា </span> ឆ្នាំ <span class="p1"> ២០២០ </span> មានលោក/លោកស្រី <span class="p1"> សុខ ចំណាន </span> មានតួនាទី&#47;ភារកិច្ចជា <span class="p1"> អគ្គនាយកក្រុមហ៊ុន </span> ជាអ្នកតំណាង ចាប់ពីពេលនេះតទៅ ហៅថា <span class="p1">«ភាគីទីភ្នាក់ងារជ្រើសរើសឯកជន» </span> និង </p>
-                        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;លោក&#47;លោកស្រីឈ្មោះ <span class="p1"> ឃាន ពិសិដ្ឋ </span> ភេទ <span class="p1"> ប្រុស </span> អាយុ <span class="p1"> ១៨ </span> សញ្ជាតិ <span> ខ្មែរ </span> មានអត្តសញ្ញាណប័ណ្ណលេខ <span class="p1"> 084 </span> ចុះថ្ងៃទី <span class="p1"> 06/01/2020 </span> ឬលិខិតឆ្លងដែនលេខ ....... ចុះថ្ងៃទី ... ខែ ..... ឆ្នាំ ..... ទីកន្លែងកំណើតភូមិ <span class="p1"> រំលេច </span> ឃុំ&#47;សង្កាត់ <span class="p1"> រំលេច </span> ស្រុក&#47;ខ័ណ្ឌ&#47;ក្រុង <span class="p1"> បាកាន </span> ខេត្ត&#47;រាជធានី <span class="p1"> ពោធិ៍សាត់ </span> ។ ព្រះរាជាណាចក្រកម្ពុជា ចាប់ពីពេលនេះតទៅហៅថា <span>« ភាគីពលករ » </span>។</p>
-                        <p style="font-weight: bold; text-align: center;">ភាគីទាំងពីរបានឯកភាព និងព្រមព្រៀងគ្នាដូចតទៅ ៖</p>
-                        <p style="font-family:Khmer OS Muol Light; font-size:18px;text-align: center;">ផ្នែកទី១<br>បទប្បញ្ញត្តិទូទៅ</p>
-                        <p><span class="p1">ប្រការ១.-</span><br>
-                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;សេវារកការងារឱ្យធ្វើនៅបរទេសគឺជាប្រព័ន្ធសេវាដែល ភាគីទីភ្នាក់ងារជ្រើសរើសឯកជន រ៉ាប់រងធ្វើ ដោយត្រូវធានាឱ្យបានថា <span class="p1">ភាគីពលករ </span> ដែលជាអ្នកប្រើសេវាទាំងឡាយនោះទទួលបានការងារសមរម្យ ដូចជា លក្ខខណ្ឌការងារល្អធ្វើនៅប្រទេស <span class="p1">ថៃ </span> ស្របតាមច្បាប់ និងបទបញ្ជាររបស់ព្រះរាជាណាចក្រកម្ពុជា និងរបស់ព្រះរាជាណាចក្រ ថៃឡង់ដ៍ ។ ការងារដែលកម្មសិក្សាការីត្រូវធ្វើមានឈ្មោះថា <span class="p1"> រោងចក្រអាហារសមុទ្រ </span> លកម្មសិក្សាការីត្រូវទទួលបានប្រាក់ឈ្នួលជាមូលដ្ឋានចំនួន <span class="p1"> ៨៥៨០ បាត </span> (សរសេរជាលេខ និងជាអក្សរ) <span class="p1"> ប្រាំបីពាន់ប្រាំរយប៉ែតសិបបាត </span> ក្នុងមួយខែៗ ។ ប្រាក់ឈ្នួលនេះមិនត្រូវទាបជាងប្រាក់ឈ្នួលអប្បបរមានៃប្រទេស ថៃ ដែលជាប្រទេសទទួលឡើយ។</p>
+
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <p style="font-size: 20px; font-family: 'Khmer OS System';">លេខៈ ARM21-TD0001</p>
+                            <h3 style="text-align: center;font-family: 'Khmer OS Muol Light';font-size: 26px;">ឧបសម្ព័ន្ធ</h3>
+                            <h3 style="text-align: center;font-family: 'Khmer OS Muol Light';font-size: 22px;">
+                                នៃប្រកាសលេខ ២៥៣ក.ប/ប្រក ចុះថ្ងៃទី២៣ ខែកញ្ញា ឆ្នាំ២០១៣ ស្តីពីការដាក់
+                                <br>
+                                ឱ្យអនុវត្តបទដ្ឋានអប្បបរមាស្តីពីកិច្ចសន្យាសេវារការងារឱ្យធ្វើនៅបរទេស
+                            </h3>
+                            <h3 style="text-align: center;font-family: 'Khmer OS Muol Light';font-size: 22px;">
+                                បទដ្ឋានអប្បបរមា
+                            </h3>
+                            <h3 style="text-align: center;font-family: 'Khmer OS Muol Light';font-size: 22px;">
+
+                                ស្តីពី<br>
+                                កិច្ចសន្យាសេវារកការងារឱ្យធ្វើ សម្រាប់ពលករខ្មែរទៅធ្វើការនៅប្រទេសថៃ
+
+                            </h3>
+
+                        </div>
+
+
                     </div>
+                    <br>
+                    <div class="row" style="font-family: 'Khmer OS Battambang'; line-height: 36px;">
+                        <div class="col-md-12">
+                            <p style="font-size: 23px;text-align: center;">កិច្ចសន្យានេះធ្វើឡើងរវាង</p>
+                            <p style="font-size: 20px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ក្រុមហ៊ុន<span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;អាន់នី រីតា បេស មេនផៅវ័រ&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                                អាសយដ្ឋានផ្ទះលេខ
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light'"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;៣២-៣៣អឺ&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ផ្លូវ
+                                <span style="text-decoration-line: underline;
+                                font-family: 'Khmer OS Muol Light'">&nbsp;&nbsp;&nbsp;&nbsp;វេងស្រេង&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ឃុំ/សង្កាត់
+                                <span style="text-decoration-line: underline;
+                                font-family: 'Khmer OS Muol Light'">&nbsp;&nbsp;&nbsp;&nbsp;ចោមចៅ&nbsp;&nbsp;&nbsp;&nbsp;</span> ស្រុក/ខណ្ឌ/ក្រុង
+                                <span style="text-decoration-line: underline;
+                                font-family: 'Khmer OS Muol Light'">&nbsp;&nbsp;&nbsp;&nbsp;ពោធិ៍សែនជ័យ&nbsp;&nbsp;&nbsp;&nbsp;</span> ខេត្ត/រាជធានី
+                                <span style="text-decoration-line: underline;
+                                font-family: 'Khmer OS Muol Light'">&nbsp;&nbsp;&nbsp;&nbsp;ភ្នំពេញ&nbsp;&nbsp;&nbsp;&nbsp; </span> ព្រះរាជាណាចក្រកម្ពុជា ដែលទទួលបានការអនុញ្ញាតតាមប្រកាសលេខ
+                                <span style="text-decoration-line: underline;
+                                font-family: 'Khmer OS Muol Light'">&nbsp;&nbsp;&nbsp; ១២៦/២០&nbsp;&nbsp;&nbsp;</span> ចុះថ្ងៃទី
+                                <span style="text-decoration-line: underline;
+                                font-family: 'Khmer OS Muol Light'">&nbsp;&nbsp;&nbsp;៣០&nbsp;&nbsp;&nbsp;</span> ខែ
+                                <span style="text-decoration-line: underline;
+                                font-family: 'Khmer OS Muol Light'">&nbsp;&nbsp;&nbsp;មីនា&nbsp;&nbsp;&nbsp;</span>ឆ្នាំ
+                                <span style="text-decoration-line: underline;
+                                font-family: 'Khmer OS Muol Light'">&nbsp;&nbsp;&nbsp;២០២០&nbsp;&nbsp;&nbsp;</span>មានលោក/លោកស្រី
+                                <span style="text-decoration-line: underline;
+                                font-family: 'Khmer OS Muol Light'">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;សុខ ចំណាន&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>មានតួនាទី/ភារកិច្ចជា
+                                <span style="text-decoration-line: underline;
+                                font-family: 'Khmer OS Muol Light'">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;អគ្គនាយកក្រុមហ៊ុន&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ជាអ្នកតំណាង ចាប់ពីពេលនេះតទៅ ហៅថា
+                                <span style="font-family: 'Khmer OS Muol Light';
+                                ">«ភាគីទីភ្នាក់ងារជ្រើសរើសឯកជន»</span> និង
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row" style="font-family: 'Khmer OS Battambang'; line-height: 37px;">
+                        <div class="col-md-12">
+                            <p style="font-size: 20px;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;លោក/លោកស្រីឈ្មោះ
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ភេទ
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> អាយុ
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>ឆ្នាំ សញ្ជាតិ
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> មានអត្តសញ្ញាណប័ណ្ណលេខ
+                                <span style="text-decoration-line: underline;  font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ចុះថ្ងៃទី
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ឬលិខិតឆ្លងដែនលេខ
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ចុះថ្ងៃទី<span></span> ខែ
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ឆ្នាំ
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ទីកន្លែងកំណើតភូមិ
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>ឃុំ/សង្កាត់
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ស្រុក/ខណ្ឌ/ក្រុង
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ខេត្ត/រាជធានី
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ។ ទូរសព្ទទំនាក់ទំនងលេខ
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ។ មានឯកសារជូនភ្ជាប់ជាមួយសម្រាប់ទុកជាឯកសារសំគាល់អត្តសញ្ញាណ ។ អាសយដ្ឋាន ផ្ទះលេខ
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ផ្លូវ
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ក្រុម
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ភូមិ
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ឃុំ/សង្កាត់
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ស្រុក/ខណ្ឌ/ក្រុង
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ខេត្ត/រាជធានី
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> ព្រះរាជាណាចក្រកម្ពុជា ចាប់ពីពេលនេះតទៅហៅថា « ភាគីពលករ » ។
+                            </p>
+
+                        </div>
+                    </div>
+                    <div class="row" style="font-family: 'Khmer OS Battambang'; line-height: 37px;">
+                        <div class="col-md-12">
+                            <p style="font-weight: bold; text-align: center;font-size: 20px;">
+                                ភាគីទាំងពីរបានឯកភាព និងព្រមព្រៀងគ្នាដូចតទៅ ៖
+                            </p>
+                            <h4 style="text-align: center;font-size: 19px; font-family: 'Khmer OS Muol Light';">ផ្នែកទី១ <br>
+                                <br> បទប្បញ្ញត្តិទូទៅ
+                            </h4>
+
+                        </div>
+                    </div>
+                    <div class="row" style="font-family: 'Khmer OS Battambang'; line-height: 37px;">
+                        <div class="col-md-12">
+                            <p style="font-size: 20px ;">
+                                <span style="font-family: 'Khmer OS Muol Light';">ប្រការ១.-</span><br>
+                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                សេវារកការងារឱ្យធ្វើនៅបរទេសគឺជាប្រព័ន្ធសេវាដែល ភាគីទីភ្នាក់ងារជ្រើសរើសឯកជន រ៉ាប់រងធ្វើ ដោយត្រូវធានាឱ្យបានថា ភាគីពលករ ដែលជាអ្នកប្រើសេវាទាំងឡាយនោះទទួលបានការងារសមរម្យ ដូចជា លក្ខខណ្ឌការងារល្អធ្វើនៅប្រទេស ថៃ ស្របតាមច្បាប់ និងបទបញ្ជាររបស់ព្រះរាជាណាចក្រកម្ពុជា និងរបស់ព្រះរាជាណាចក្រ ថៃឡង់ដ៍ ។ ការងារដែលកម្មសិក្សាការីត្រូវធ្វើមានឈ្មោះថា
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>ដែលកម្មសិក្សាការីត្រូវទទួលបានប្រាក់ឈ្នួលជាមូលដ្ឋានចំនួន
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;៨៥៨០ បាត&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;​&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>សរសេរជាលេខ និងជាអក្សរ)
+                                <span style="text-decoration-line: underline; font-family: 'Khmer OS Muol Light';"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ប្រាំបីពាន់ប្រាំរយប៉ែតសិបបាត&nbsp;&nbsp;&nbsp;&nbsp;</span> ក្នុងមួយខែៗ ។ ប្រាក់ឈ្នួលនេះមិនត្រូវទាបជាងប្រាក់ឈ្នួលអប្បបរមានៃប្រទេស ថៃ ដែលជាប្រទេសទទួលឡើយ។
+                            </p>
+
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img src="<?php echo base_url('images/aaa.png') ?>" alt="" width="100%">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img src="<?php echo base_url('images/bbbb.png') ?>" alt="">
+                        </div>
+                    </div>
+                    <div class="row" style="position: relative;">
+                        <div class="col-md-12">
+                            <img src="<?php echo base_url('images/cc.png') ?>" alt="" style="position: relative;">
+                            <span style=" position: absolute;margin-top: 870px; margin-left: -685px;z-index: 2;">222</span>
+                            <span style=" position: absolute;">២5</span>
+                            <span style=" position: absolute;">២៧០០</span>
+                            <span style=" position: absolute;">២៧sdfdf</span>
+                        </div>
+                    </div>
+
+
+
                 </section>
                 <!-- /.content -->
             </div>
